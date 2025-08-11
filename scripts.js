a010583969d6c5f2809f7fa4c6b655fb20f7751d
Rodney St Cloud <hoyrod1@aol.com>
2025-08-11 12:11:15 -0400

    Created the fondational files index.html, scripts.js and server.js to build the video chat. Dependencies installed are express, socket.io and mkcert

--- scripts.js
+++ scripts.js
@@ -1,15 +1,32 @@
-// import { resolve } from "path";
-// import { rejects } from "assert";
-// const { resolve } = require("path");
-// const { rejects } = require("assert");
+const userName = "Rodney-" + Math.floor(Math.random() * 1000);
+const passWord = "X";
+document.querySelector("#user-name").innerHTML = userName;
+//==========================================================================//
 
+//==========================================================================//
+// FIRST STEP: io.connect RUNS WHEN SOMEONE BROWSES TO THIS URL //
+// NEXT STEP: GO TO server.js TO io.on() //
+const socket = io.connect("https://localhost:7070/", {
+  auth: {
+    userName,
+    passWord,
+  },
+});
+//==========================================================================//
+
+//==========================================================================//
 const localVideoEl = document.querySelector("#local-video");
 const remoteVideoEl = document.querySelector("#remote-video");
+//==========================================================================//
 
+//==========================================================================//
 let localStream; //a var to hold the local video stream
 let remoteStream; //a var to hold the remote video stream
 let peerConnection; //the peerConnection that the two clients use to talk
+let didIOffer = false;
+//==========================================================================//
 
+//==========================================================================//
 let peerConfiguration = {
   iceServers: [
     {
@@ -17,39 +34,69 @@ let peerConfiguration = {
     },
   ],
 };
+//==========================================================================//
 
+//==========================================================================//
 //when a client initiates a call
 const call = async (e) => {
+  //------------------------------------------------------------------------//
   const stream = await navigator.mediaDevices.getUserMedia({
     video: true,
     audio: true,
   });
+  //------------------------------------------------------------------------//
+
+  //------------------------------------------------------------------------//
   localVideoEl.srcObject = stream;
   localStream = stream;
+  //------------------------------------------------------------------------//
 
+  //------------------------------------------------------------------------//
   await createPeerConnection();
+  //------------------------------------------------------------------------//
 
+  //------------------------------------------------------------------------//
   try {
     console.log("Creating offer...");
     const offer = await peerConnection.createOffer();
-    console.log(offer);
+    // console.log(offer);
+    peerConnection.setLocalDescription(offer);
+    didIOffer = true;
+    socket.emit("newOffer", offer); // This sends offer to signaling server //
   } catch (error) {
     console.log(error);
   }
+  //------------------------------------------------------------------------//
 };
+//==========================================================================//
 
+//==========================================================================//
 const createPeerConnection = () => {
   return new Promise(async (resolve, rejects) => {
+    //----------------------------------------------------------------------//
     peerConnection = await new RTCPeerConnection(peerConfiguration);
+    //----------------------------------------------------------------------//
     localStream.getTracks().forEach((track) => {
       peerConnection.addTrack(track, localStream);
     });
-    peerConnection.addEventListener("icecanidate", (e) => {
-      console.log(`.......Ice Canidate Found.......`);
-      console.log(e);
+    //----------------------------------------------------------------------//
+    peerConnection.addEventListener("icecandidate", (e) => {
+      // console.log(e);
+      if (e.candidate) {
+        socket.emit("sendIceCandidateToSignalingServer", {
+          iceCandidate: e.candidate,
+          iceUserName: userName,
+          didIOffer,
+        });
+      }
     });
+    //----------------------------------------------------------------------//
     resolve();
+    //----------------------------------------------------------------------//
   });
 };
+//==========================================================================//
 
+//==========================================================================//
 document.querySelector("#call").addEventListener("click", call);
+//==========================================================================//
--- server.js
+++ server.js
@@ -1,4 +1,103 @@
+const fs = require("fs");
+//============================================================//
+const https = require("https");
+//============================================================//
 const express = require("express");
+//============================================================//
 const app = express();
+//============================================================//
+const socketio = require("socket.io");
+//============================================================//
 app.use(express.static(__dirname));
-app.listen(7070);
+//============================================================//
+// MADE KEY AND CERT TO RUN HTTPS //
+const key = fs.readFileSync("cert.key");
+const cert = fs.readFileSync("cert.crt");
+//============================================================//
+// WHEN USING HTTPS //
+const expressServer = https.createServer({ key, cert }, app);
+//============================================================//
+const io = socketio(expressServer);
+//============================================================//
+// THIS WILL CONTAINS THE VISTING URL INFORMATION
+const offers = [
+  // offererUserName
+  // offer
+  // offerIceCandidates
+  // answer
+  // answererUserName
+  // answererIceCandidates
+];
+//============================================================//
+// THIS CONTAINS THE VISTING URL's SOCKET ID & USERNAME
+const connectedSockets = [
+  // username,
+  // socketId
+];
+//============================================================//
+
+//============================================================//
+// SECOND STEP: SOMEONE HAS CONNECTED TO THE SOCKET.IO SERVER //
+io.on("connection", (socket) => {
+  //----------------------------------------------------------//
+  const userName = socket.handshake.auth.userName;
+  const passWord = socket.handshake.auth.passWord;
+  //----------------------------------------------------------//
+  if (passWord !== "X") {
+    socket.disconnect(true);
+    return;
+  }
+  //----------------------------------------------------------//
+  // PUSH THE VISITOR TO connectedSockets[] ARRAY
+  connectedSockets.push({
+    socketId: socket.id,
+    userName,
+  });
+  //----------------------------------------------------------//
+
+  //----------------------------------------------------------//
+  if (offers.length) {
+    socket.emit("availableOffers", offers);
+  }
+  //----------------------------------------------------------//
+
+  //---------------------------------------------------------------//
+  // LISTENING FOR NEW OFFER AND EMIT/SEND THE VISTORS INFORMATION
+  socket.on("newOffer", (newOffer) => {
+    //-------------------------------------------------------------//
+    // PUSH AVAILABLE VISITORS INFORMTION TO offers[] ARRAY
+    offers.push({
+      offererUserName: userName,
+      offer: newOffer,
+      offerIceCandidates: [],
+      answer: null,
+      answererUserName: null,
+      answererIceCandidates: [],
+    });
+    // console.log(newOffer.sdp.slice(50));
+    //-------------------------------------------------------------//
+    // SEND THE CONNECTED SOCKETS TO EVERYONE CONNECTED EXCEPT CALLER
+    socket.broadcast.emit("newOfferAwaiting", offers.slice(-1));
+  });
+  //----------------------------------------------------------//
+
+  //----------------------------------------------------------//
+  socket.on("sendIceCandidateToSignalingServer", (iceCandidateObj) => {
+    // console.log(iceCandidateObj);
+    const { didIOffer, iceUsername, iceCandidate } = iceCandidateObj;
+    // console.log(iceCandidate);
+    if (didIOffer) {
+      const offerInOffers = offers.find((o) => o.userName === iceUsername);
+      if (offerInOffers) {
+        offerInOffers.offerIceCandidates.push(iceCandidate);
+      }
+    }
+    console.log(offers);
+  });
+  //----------------------------------------------------------//
+});
+//============================================================//
+
+//============================================================//
+// APPLICATION LISTENING ON PORT 7070//
+expressServer.listen(7070);
